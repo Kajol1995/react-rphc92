37c864040f9e068fa5244f36689bb6f23728e868
Kajol1995 <kajollachake95@gmail.com>
2022-01-04 15:33:58 +0530

    2 files modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import Form from './Form';
+import Button from './Button';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello!!!</h1>
-      <p>Myself Kajol... :)</p>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <div>
+            <Link to="/Form" path="/Form">
+              Form
+            </Link>
+          </div>
+          <div>
+            <Link to="/Button" path="/Button">
+              Button
+            </Link>
+            <Route exact path="/" component={App}></Route>
+            <Route exact path="/Form" component={Form}></Route>
+            <Route exact path="/Button" component={Button}></Route>
+          </div>
+        </Routes>
+      </Router>
+    </>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <div>
+      <h1>Hello, I am Kajol...</h1>
+    </div>
+  );
+};
