c5d44804a9db5a4aac8bb175890d0cee2a7998cc
Kajol1995 <kajollachake95@gmail.com>
2022-01-04 17:48:53 +0530

    1 file modified

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import mobile from './Assets/mobile.jfif';
+import download2 from './Assets/download2.jfif';
+import OIP1 from './Assets/OIP1.jfif';
+import OIP3 from './Assets/OIP3.jfif';
+import OIP4 from './Assets/OIP4.jfif';
+import OIP5 from './Assets/OIP5.jfif';
+
 import './Form.css';
 
 const Form = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="image">
+      <h1> Hello Form...</h1>
+      <img src={mobile} alt="This is a Mobile"></img>
+      <img src={download2} alt="This is a mobile too"></img>
+      <img src={OIP1} alt="This is a mobile too"></img>
+      <img src={OIP3} alt="This is a mobile too"></img>
+      <img src={OIP4} alt="This is a mobile too"></img>
+      <img src={OIP5} alt="This is a mobile too"></img>
     </div>
   );
 };
